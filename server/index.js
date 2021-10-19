d5e422e1c6dbdffe837c3a46307740edfbce9300
Vishwash Bajpai <vishwash.bajpai87@gmail.com>
2021-10-19 23:00:28 +0530

    added the basic crud api for post schema

---
+++ server/index.js
@@ -0,0 +1,24 @@
+
+import express from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import dotenv from 'dotenv'
+import postRoutes from './routes/posts.js';
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({
+  extended: true
+}));
+
+app.use(cors());
+
+app.use('/posts', postRoutes);
+
+const PORT = process.env.PORT|| 5000;
+
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
+  .catch((error) => console.log(`${error} did not connect`));
